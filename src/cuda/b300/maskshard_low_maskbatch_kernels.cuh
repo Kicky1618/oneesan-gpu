e8ee0f33cde1c2ec2d622a5c5f26c13fa7f21bf1
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-25 18:38:38 +0900

    dispatch(v0.48): select rebuilt LOW batch metadata

--- src/cuda/b300/maskshard_low_maskbatch_kernels.cuh
+++ src/cuda/b300/maskshard_low_maskbatch_kernels.cuh
@@ -1,7 +1,9 @@
 #pragma once
 
 #ifdef MASKSHARD_LOW_MASKBATCH_CTA_CONFIG_CACHE
-#ifdef MASKSHARD_LOW_MASKBATCH_COMPACT_DYNAMIC
+#ifdef MASKSHARD_LOW_MASKBATCH_REBUILD_DYNAMIC
+#include "maskshard_low_maskbatch_kernels_rebuild.cuh"
+#elif defined(MASKSHARD_LOW_MASKBATCH_COMPACT_DYNAMIC)
 #include "maskshard_low_maskbatch_kernels_compact.cuh"
 #else
 #include "maskshard_low_maskbatch_kernels_cached.cuh"
