7f6ac74316769fc8deb43684b6569ebbcdd6fdf8
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-25 18:34:11 +0900

    dispatch(v0.47): select compact CTA LOW batch kernels

--- src/cuda/b300/maskshard_low_maskbatch_kernels.cuh
+++ src/cuda/b300/maskshard_low_maskbatch_kernels.cuh
@@ -1,7 +1,11 @@
 #pragma once
 
 #ifdef MASKSHARD_LOW_MASKBATCH_CTA_CONFIG_CACHE
+#ifdef MASKSHARD_LOW_MASKBATCH_COMPACT_DYNAMIC
+#include "maskshard_low_maskbatch_kernels_compact.cuh"
+#else
 #include "maskshard_low_maskbatch_kernels_cached.cuh"
+#endif
 #else
 #include "maskshard_low_maskbatch_kernels_global.cuh"
 #endif
