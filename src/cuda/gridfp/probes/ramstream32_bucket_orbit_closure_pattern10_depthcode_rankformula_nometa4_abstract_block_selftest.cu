446262f85867f96fd2599f3409df68d0531c63f5
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-29 07:53:59 +0900

    test(gridfp): wrap abstract nometa block selftest

---
+++ src/cuda/gridfp/probes/ramstream32_bucket_orbit_closure_pattern10_depthcode_rankformula_nometa4_abstract_block_selftest.cu
@@ -0,0 +1,17 @@
+#pragma push_macro("main")
+#undef main
+#define main p10dc_rankformula_nometa4_abstract_base_selftest_main
+#include "ramstream32_bucket_orbit_closure_pattern10_depthcode_rankformula_nometa4_abstract_selftest.cu"
+#pragma pop_macro("main")
+
+int main() {
+    const int rc = p10dc_rankformula_nometa4_abstract_base_selftest_main();
+    if (rc == 0) {
+        std::cout << "rankformula-nometa4-abstract-block-selftest compiled_block="
+                  << P10DC_RANKFORMULA_NOMETA4_BLOCK
+                  << " max_locator_steps_bound="
+                  << (P10DC_RANKFORMULA_NOMETA4_BLOCK - 1u)
+                  << " wrapper_ok=1\n";
+    }
+    return rc;
+}
