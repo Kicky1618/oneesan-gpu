98c7a6e3dc70fdcd3592b7aaf25de6fe41293ba6
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-28 12:30:09 +0900

    perf(gridfp): add affine prekey rankstream HIGH sum

---
+++ src/cuda/gridfp/ramstream32_bucket_closure_pattern10_depthcode_delta_direct_affine_prekey_rankstream.cuh
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "ramstream32_bucket_closure_pattern10_depthcode_delta_direct_affine.cuh"
+#include "ramstream32_bucket_closure_cross5_rankstream.cuh"
+
+__device__ __forceinline__ Count p10dc_direct_resolved_high_plan_sum_cross5_prekey_rankstream(
+    const P10DCDirectHighResolvedCtx& c, const BucketPhysicalBlock& db, uint32_t lr
+) {
+#if GPU_DIRECT_PM_ACCUM
+    uint64_t sum = 0;
+#else
+    Count sum = 0;
+#endif
+#pragma unroll
+    for (uint32_t i = 0; i < BKCZ_MAX_LOCAL; ++i) {
+        if (i < c.local_n) {
+            Count v = c.local_base[i][lr];
+#if GPU_DIRECT_PM_ACCUM
+            sum += uint64_t(v);
+#else
+            sum = gpu_direct_add(sum, v);
+#endif
+        }
+    }
+    uint32_t depth = c.cross_depth;
+    if (depth) {
+        uint32_t key = p10dc_low_prekey_fixed(db.hs, lr);
+#if GPU_DIRECT_PM_ACCUM
+        sum += p10dc_resolved_low_preimages_cross5_rankstream_fixed(
+            db.hs, lr, key, depth, c.cross_base);
+#else
+        sum = gpu_direct_add(
+            sum, p10dc_resolved_low_preimages_cross5_rankstream_fixed(
+                     db.hs, lr, key, depth, c.cross_base));
+#endif
+    }
+#if GPU_DIRECT_PM_ACCUM
+    return gpu_direct_pm_reduce_u64(sum);
+#else
+    return sum;
+#endif
+}
