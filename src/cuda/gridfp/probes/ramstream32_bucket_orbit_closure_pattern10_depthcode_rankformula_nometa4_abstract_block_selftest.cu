8a0f7075852935642f1db6d1d6707d00316378c7
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-29 08:35:33 +0900

    test(gridfp): report cooperative locator unroll

--- src/cuda/gridfp/probes/ramstream32_bucket_orbit_closure_pattern10_depthcode_rankformula_nometa4_abstract_block_selftest.cu
+++ src/cuda/gridfp/probes/ramstream32_bucket_orbit_closure_pattern10_depthcode_rankformula_nometa4_abstract_block_selftest.cu
@@ -11,6 +11,7 @@ int main() {
                   << P10DC_RANKFORMULA_NOMETA4_BLOCK
                   << " warpshare=" << P10DC_RANKFORMULA_NOMETA_WARPSHARE
                   << " coopgroup=" << P10DC_RANKFORMULA_NOMETA_COOPGROUP
+                  << " coop_unroll=" << P10DC_RANKFORMULA_NOMETA_COOP_UNROLL
                   << " max_locator_steps_bound="
                   << (P10DC_RANKFORMULA_NOMETA4_BLOCK - 1u)
                   << " wrapper_ok=1\n";
