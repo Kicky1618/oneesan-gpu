2d9f3ec7301d87e674cce28f858027b4a65064bb
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-29 13:49:27 +0900

    gridfp: route rankchunk32 through direct-mask hot path

--- src/cuda/gridfp/ramstream32_bucket_orbit_closure_pattern10_depthcode_warpstriped_delta_direct_affine_rankchunk32_cross5.cuh
+++ src/cuda/gridfp/ramstream32_bucket_orbit_closure_pattern10_depthcode_warpstriped_delta_direct_affine_rankchunk32_cross5.cuh
@@ -1,13 +1,27 @@
 #pragma once
 
+#ifndef P10DC_RANKCHUNK32_DIRECTMASK
+#define P10DC_RANKCHUNK32_DIRECTMASK 0
+#endif
+static_assert(P10DC_RANKCHUNK32_DIRECTMASK == 0 || P10DC_RANKCHUNK32_DIRECTMASK == 1,
+              "P10DC_RANKCHUNK32_DIRECTMASK must be 0 or 1");
+
+#if P10DC_RANKCHUNK32_DIRECTMASK
+#include "ramstream32_bucket_closure_pattern10_depthcode_delta_direct_affine_rankchunk32_directmask.cuh"
+#else
 #include "ramstream32_bucket_closure_pattern10_depthcode_delta_direct_affine_rankchunk32.cuh"
+#endif
 
 #define P10DC_WARPSTRIPED_CTX P10DCDirectHighResolvedCtx
 #define P10DC_WARPSTRIPED_PREPARE_FORWARD(c,payload,loc,p,ss,js,ds,sr,jr,dr) \
     p10dc_prepare_forward_high_delta_direct_affine((c),(payload),(loc),(p),(ss),(js),(ds),(sr),(jr),(dr))
 #define P10DC_WARPSTRIPED_PREPARE_REVERSE(c,payload,loc,plan_db,p,edge,ss,js,ds,sr,jr,dr) \
     p10dc_prepare_reverse_high_delta_direct_affine((c),(payload),(loc),(plan_db),(p),(edge),(ss),(js),(ds),(sr),(jr),(dr))
+#if P10DC_RANKCHUNK32_DIRECTMASK
+#define p10dc_resolved_high_plan_sum p10dc_direct_resolved_high_plan_sum_rankchunk32_directmask
+#else
 #define p10dc_resolved_high_plan_sum p10dc_direct_resolved_high_plan_sum_cross5_rankchunk32
+#endif
 #define bucket_high_orbit_closure_pattern10_depthcode_warpstriped_kernel \
     bucket_high_orbit_closure_pattern10_depthcode_warpstriped_delta_direct_affine_rankchunk32_cross5_kernel
 #define bucket_reverse_high_pattern10_depthcode_warpstriped_kernel \
