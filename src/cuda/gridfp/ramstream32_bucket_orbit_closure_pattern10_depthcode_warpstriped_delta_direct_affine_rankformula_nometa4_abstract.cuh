b2cc9aa0827978332f5efc5e519769ff878bb073
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-29 15:31:13 +0900

    perf(gridfp): select overlap-local pair pipeline

--- src/cuda/gridfp/ramstream32_bucket_orbit_closure_pattern10_depthcode_warpstriped_delta_direct_affine_rankformula_nometa4_abstract.cuh
+++ src/cuda/gridfp/ramstream32_bucket_orbit_closure_pattern10_depthcode_warpstriped_delta_direct_affine_rankformula_nometa4_abstract.cuh
@@ -18,14 +18,29 @@
 #ifndef P10DC_RANKFORMULA_CPASYNC_LOCAL_PAIR
 #define P10DC_RANKFORMULA_CPASYNC_LOCAL_PAIR 0
 #endif
+#ifndef P10DC_RANKFORMULA_CPASYNC_OVERLAP_LOCAL_PAIR
+#define P10DC_RANKFORMULA_CPASYNC_OVERLAP_LOCAL_PAIR 0
+#endif
 static_assert(P10DC_RANKFORMULA_CPASYNC_LOCAL_PAIR == 0 ||
               P10DC_RANKFORMULA_CPASYNC_LOCAL_PAIR == 1,
               "P10DC_RANKFORMULA_CPASYNC_LOCAL_PAIR must be 0 or 1");
+static_assert(P10DC_RANKFORMULA_CPASYNC_OVERLAP_LOCAL_PAIR == 0 ||
+              P10DC_RANKFORMULA_CPASYNC_OVERLAP_LOCAL_PAIR == 1,
+              "P10DC_RANKFORMULA_CPASYNC_OVERLAP_LOCAL_PAIR must be 0 or 1");
+static_assert(!(P10DC_RANKFORMULA_CPASYNC_LOCAL_PAIR &&
+                P10DC_RANKFORMULA_CPASYNC_OVERLAP_LOCAL_PAIR),
+              "local-cpasync and overlap-local pair modes are isolated A/B paths");
+static_assert(!P10DC_RANKFORMULA_CPASYNC_OVERLAP_LOCAL_PAIR ||
+              !P10DC_RANKFORMULA_QUAD_MLP,
+              "overlap-local pair is isolated from QUAD_MLP");
 #if P10DC_RANKFORMULA_PRECTX_FORWARD || P10DC_RANKFORMULA_PRECTX_REVERSE
 #include "ramstream32_bucket_precomputed_forward_high_ctx.cuh"
 #endif
 #if P10DC_RANKFORMULA_CPASYNC_LOCAL_PAIR
 #include "ramstream32_bucket_closure_pattern10_depthcode_rankformula_local_cpasync_pair.cuh"
+#endif
+#if P10DC_RANKFORMULA_CPASYNC_OVERLAP_LOCAL_PAIR
+#include "ramstream32_bucket_closure_pattern10_depthcode_rankformula_overlap_local_pair.cuh"
 #endif
 
 #if P10DC_RANKFORMULA_GATHER_MLP
@@ -57,7 +72,11 @@ static_assert(P10DC_RANKFORMULA_CPASYNC_LOCAL_PAIR == 0 ||
 #if P10DC_RANKFORMULA_PAIR_MLP
 static_assert(P10DC_WARPSTRIPED_COL_ILP == 2 || P10DC_WARPSTRIPED_COL_ILP == 4,
               "PAIR_MLP requires COL_ILP=2 or 4");
-#if P10DC_RANKFORMULA_CPASYNC_LOCAL_PAIR
+#if P10DC_RANKFORMULA_CPASYNC_OVERLAP_LOCAL_PAIR
+#define P10DC_WARPSTRIPED_PLAN_SUM_PAIR(c,db,lr0,lr1,out0,out1) \
+    p10dc_direct_resolved_high_plan_sum_pair_overlap_local( \
+        (c),(db),(lr0),(lr1),(out0),(out1))
+#elif P10DC_RANKFORMULA_CPASYNC_LOCAL_PAIR
 #define P10DC_WARPSTRIPED_PLAN_SUM_PAIR(c,db,lr0,lr1,out0,out1) \
     p10dc_direct_resolved_high_plan_sum_pair_local_cpasync( \
         (c),(db),(lr0),(lr1),(out0),(out1))
