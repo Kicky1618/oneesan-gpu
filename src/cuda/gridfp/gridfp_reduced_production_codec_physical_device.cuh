be146745cf882e621f24f672eb75238d9cc2d213
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-29 12:04:14 +0900

    gridfp: remove legacy codec upload sinks in physical modes

--- src/cuda/gridfp/gridfp_reduced_production_codec_physical_device.cuh
+++ src/cuda/gridfp/gridfp_reduced_production_codec_physical_device.cuh
@@ -30,17 +30,12 @@ __device__ __forceinline__ int codec_physical_sym_row_base_device(int n) {
 __constant__ Rank64 RP_CHOOSE[RP_MAX_W + 1][RP_MAX_W + 1];
 static constexpr int RP_CODEC_PHYSICAL_CHOOSE_BYTES =
     (RP_MAX_W + 1) * (RP_MAX_W + 1) * int(sizeof(Rank64));
-static constexpr int RP_CODEC_PHYSICAL_CHOOSE_UPLOAD_SINK_BYTES = 0;
 #elif RP_EXPERIMENTAL_CODEC_CHOOSE_PHYSICAL_MODE == 1
-// Preserve the existing host cudaMemcpyToSymbol call without retaining the
-// legacy table in constant memory. Device code is redirected to the u32 table.
-__device__ Rank64 RP_CHOOSE[RP_MAX_W + 1][RP_MAX_W + 1];
 __device__ __constant__ std::uint32_t RP_CODEC_PHYSICAL_CHOOSE_SYM_U32[225] = {
 #include "gridfp_reduced_production_choose_sym_u32_values.inc"
 };
 static_assert(sizeof(RP_CODEC_PHYSICAL_CHOOSE_SYM_U32) == 900);
 static constexpr int RP_CODEC_PHYSICAL_CHOOSE_BYTES = 900;
-static constexpr int RP_CODEC_PHYSICAL_CHOOSE_UPLOAD_SINK_BYTES = 6728;
 struct CodecPhysicalChooseRowProxy {
     int n = 0;
     __device__ __forceinline__ Rank64 operator[](int k) const {
@@ -52,13 +47,11 @@ struct CodecPhysicalChooseRowProxy {
     }
 };
 #elif RP_EXPERIMENTAL_CODEC_CHOOSE_PHYSICAL_MODE == 2
-__device__ Rank64 RP_CHOOSE[RP_MAX_W + 1][RP_MAX_W + 1];
 __device__ __constant__ std::uint32_t RP_CODEC_PHYSICAL_CHOOSE_TRI_U32[435] = {
 #include "gridfp_reduced_production_choose_tri_u32_values.inc"
 };
 static_assert(sizeof(RP_CODEC_PHYSICAL_CHOOSE_TRI_U32) == 1740);
 static constexpr int RP_CODEC_PHYSICAL_CHOOSE_BYTES = 1740;
-static constexpr int RP_CODEC_PHYSICAL_CHOOSE_UPLOAD_SINK_BYTES = 6728;
 struct CodecPhysicalChooseRowProxy {
     int n = 0;
     __device__ __forceinline__ Rank64 operator[](int k) const {
@@ -67,14 +60,12 @@ struct CodecPhysicalChooseRowProxy {
     }
 };
 #else
-__device__ Rank64 RP_CHOOSE[RP_MAX_W + 1][RP_MAX_W + 1];
 __device__ __constant__ std::uint32_t
 RP_CODEC_PHYSICAL_CHOOSE_FULL_U32[RP_MAX_W + 1][RP_MAX_W + 1] = {
 #include "gridfp_reduced_production_choose_full_u32_values.inc"
 };
 static_assert(sizeof(RP_CODEC_PHYSICAL_CHOOSE_FULL_U32) == 3364);
 static constexpr int RP_CODEC_PHYSICAL_CHOOSE_BYTES = 3364;
-static constexpr int RP_CODEC_PHYSICAL_CHOOSE_UPLOAD_SINK_BYTES = 6728;
 struct CodecPhysicalChooseRowProxy {
     int n = 0;
     __device__ __forceinline__ Rank64 operator[](int k) const {
@@ -99,15 +90,12 @@ struct CodecPhysicalChooseProxy {
 __constant__ Rank64 RP_PRIMITIVE[RP_MAX_W + 1][RP_MAX_W + 2];
 static constexpr int RP_CODEC_PHYSICAL_PRIMITIVE_BYTES =
     (RP_MAX_W + 1) * (RP_MAX_W + 2) * int(sizeof(Rank64));
-static constexpr int RP_CODEC_PHYSICAL_PRIMITIVE_UPLOAD_SINK_BYTES = 0;
 #elif RP_EXPERIMENTAL_CODEC_PRIMITIVE_PHYSICAL_MODE == 1
-__device__ Rank64 RP_PRIMITIVE[RP_MAX_W + 1][RP_MAX_W + 2];
 __device__ __constant__ std::uint32_t RP_CODEC_PHYSICAL_PRIMITIVE_SYM_U32[225] = {
 #include "gridfp_reduced_production_primitive_sym_u32_values.inc"
 };
 static_assert(sizeof(RP_CODEC_PHYSICAL_PRIMITIVE_SYM_U32) == 900);
 static constexpr int RP_CODEC_PHYSICAL_PRIMITIVE_BYTES = 900;
-static constexpr int RP_CODEC_PHYSICAL_PRIMITIVE_UPLOAD_SINK_BYTES = 6960;
 struct CodecPhysicalPrimitiveRowProxy {
     int rem = 0;
     __device__ __forceinline__ Rank64 operator[](int h) const {
@@ -118,7 +106,6 @@ struct CodecPhysicalPrimitiveRowProxy {
     }
 };
 #else
-__device__ Rank64 RP_PRIMITIVE[RP_MAX_W + 1][RP_MAX_W + 2];
 __device__ __constant__ std::uint32_t
 RP_CODEC_PHYSICAL_PRIMITIVE_FULL_U32[RP_MAX_W + 1][RP_MAX_W + 2] = {
 #include "gridfp_reduced_production_primitive_full_u32_values_0_9.inc"
@@ -129,7 +116,6 @@ RP_CODEC_PHYSICAL_PRIMITIVE_FULL_U32[RP_MAX_W + 1][RP_MAX_W + 2] = {
 };
 static_assert(sizeof(RP_CODEC_PHYSICAL_PRIMITIVE_FULL_U32) == 3480);
 static constexpr int RP_CODEC_PHYSICAL_PRIMITIVE_BYTES = 3480;
-static constexpr int RP_CODEC_PHYSICAL_PRIMITIVE_UPLOAD_SINK_BYTES = 6960;
 struct CodecPhysicalPrimitiveRowProxy {
     int rem = 0;
     __device__ __forceinline__ Rank64 operator[](int h) const {
@@ -152,6 +138,3 @@ struct CodecPhysicalPrimitiveProxy {
 
 static constexpr int RP_CODEC_PHYSICAL_TABLE_BYTES =
     RP_CODEC_PHYSICAL_CHOOSE_BYTES + RP_CODEC_PHYSICAL_PRIMITIVE_BYTES;
-static constexpr int RP_CODEC_PHYSICAL_UPLOAD_SINK_BYTES =
-    RP_CODEC_PHYSICAL_CHOOSE_UPLOAD_SINK_BYTES +
-    RP_CODEC_PHYSICAL_PRIMITIVE_UPLOAD_SINK_BYTES;
