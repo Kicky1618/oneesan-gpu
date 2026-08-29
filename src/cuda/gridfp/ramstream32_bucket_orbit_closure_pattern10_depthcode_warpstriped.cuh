0fe36aab9dbe9416585cbd2e395c4884cb962db0
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-29 15:29:29 +0900

    perf(gridfp): issue four HIGH columns through quad MLP

--- src/cuda/gridfp/ramstream32_bucket_orbit_closure_pattern10_depthcode_warpstriped.cuh
+++ src/cuda/gridfp/ramstream32_bucket_orbit_closure_pattern10_depthcode_warpstriped.cuh
@@ -128,23 +128,32 @@ __global__ void bucket_high_orbit_closure_pattern10_depthcode_warpstriped_kernel
 #endif
                     }
                 }
+#ifdef P10DC_WARPSTRIPED_PLAN_SUM_QUAD
+                if (live[3]) {
+                    P10DC_WARPSTRIPED_PLAN_SUM_QUAD(
+                        c, db, lr[0], lr[1], lr[2], lr[3],
+                        extra[0], extra[1], extra[2], extra[3]);
+                } else
+#endif
+                {
 #ifdef P10DC_WARPSTRIPED_PLAN_SUM_PAIR
 #pragma unroll
-                for (int t = 0; t < P10DC_WARPSTRIPED_COL_ILP; t += 2) {
-                    if (!live[t]) continue;
-                    if (live[t + 1]) {
-                        P10DC_WARPSTRIPED_PLAN_SUM_PAIR(
-                            c, db, lr[t], lr[t + 1], extra[t], extra[t + 1]);
-                    } else {
-                        extra[t] = p10dc_resolved_high_plan_sum(c, db, lr[t]);
+                    for (int t = 0; t < P10DC_WARPSTRIPED_COL_ILP; t += 2) {
+                        if (!live[t]) continue;
+                        if (live[t + 1]) {
+                            P10DC_WARPSTRIPED_PLAN_SUM_PAIR(
+                                c, db, lr[t], lr[t + 1], extra[t], extra[t + 1]);
+                        } else {
+                            extra[t] = p10dc_resolved_high_plan_sum(c, db, lr[t]);
+                        }
                     }
-                }
 #else
 #pragma unroll
-                for (int t = 0; t < P10DC_WARPSTRIPED_COL_ILP; ++t) {
-                    if (live[t]) extra[t] = p10dc_resolved_high_plan_sum(c, db, lr[t]);
-                }
+                    for (int t = 0; t < P10DC_WARPSTRIPED_COL_ILP; ++t) {
+                        if (live[t]) extra[t] = p10dc_resolved_high_plan_sum(c, db, lr[t]);
+                    }
 #endif
+                }
 #pragma unroll
                 for (int t = 0; t < P10DC_WARPSTRIPED_COL_ILP; ++t) {
                     if (!live[t]) continue;
@@ -264,23 +273,32 @@ __global__ void bucket_reverse_high_pattern10_depthcode_warpstriped_kernel(int p
 #endif
                     }
                 }
+#ifdef P10DC_WARPSTRIPED_PLAN_SUM_QUAD
+                if (live[3]) {
+                    P10DC_WARPSTRIPED_PLAN_SUM_QUAD(
+                        c, edge ? xb : db, lr[0], lr[1], lr[2], lr[3],
+                        extra[0], extra[1], extra[2], extra[3]);
+                } else
+#endif
+                {
 #ifdef P10DC_WARPSTRIPED_PLAN_SUM_PAIR
 #pragma unroll
-                for (int t = 0; t < P10DC_WARPSTRIPED_COL_ILP; t += 2) {
-                    if (!live[t]) continue;
-                    if (live[t + 1]) {
-                        P10DC_WARPSTRIPED_PLAN_SUM_PAIR(
-                            c, edge ? xb : db, lr[t], lr[t + 1], extra[t], extra[t + 1]);
-                    } else {
-                        extra[t] = p10dc_resolved_high_plan_sum(c, edge ? xb : db, lr[t]);
+                    for (int t = 0; t < P10DC_WARPSTRIPED_COL_ILP; t += 2) {
+                        if (!live[t]) continue;
+                        if (live[t + 1]) {
+                            P10DC_WARPSTRIPED_PLAN_SUM_PAIR(
+                                c, edge ? xb : db, lr[t], lr[t + 1], extra[t], extra[t + 1]);
+                        } else {
+                            extra[t] = p10dc_resolved_high_plan_sum(c, edge ? xb : db, lr[t]);
+                        }
                     }
-                }
 #else
 #pragma unroll
-                for (int t = 0; t < P10DC_WARPSTRIPED_COL_ILP; ++t) {
-                    if (live[t]) extra[t] = p10dc_resolved_high_plan_sum(c, edge ? xb : db, lr[t]);
-                }
+                    for (int t = 0; t < P10DC_WARPSTRIPED_COL_ILP; ++t) {
+                        if (live[t]) extra[t] = p10dc_resolved_high_plan_sum(c, edge ? xb : db, lr[t]);
+                    }
 #endif
+                }
 #pragma unroll
                 for (int t = 0; t < P10DC_WARPSTRIPED_COL_ILP; ++t) {
                     if (!live[t]) continue;
