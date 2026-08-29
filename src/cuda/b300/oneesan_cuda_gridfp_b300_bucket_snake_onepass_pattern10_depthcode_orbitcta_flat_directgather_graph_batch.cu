24b823ee5d58e6dc6c30ed421c449924bc1ab5dd
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-29 18:43:50 +0900

    perf(b300): expose flat persistent orbit CTA backend

---
+++ src/cuda/b300/oneesan_cuda_gridfp_b300_bucket_snake_onepass_pattern10_depthcode_orbitcta_flat_directgather_graph_batch.cu
@@ -0,0 +1,54 @@
+#ifndef BUCKET_SNAKE_REVERSE_FUSED
+#define BUCKET_SNAKE_REVERSE_FUSED 1
+#endif
+
+#include "../gridfp/ramstream32_bucket_orbit_closure_pattern10_depthcode_orbitcta_flat_delta_direct_affine_rankformula_nometa4_abstract_graph.cuh"
+#include "../gridfp/ramstream32_reverse_build_release.hpp"
+#if P10DC_RANKFORMULA_DIRECTGATHER64
+#include "../gridfp/ramstream32_bucket_low_rankformula_directgather64.cuh"
+#if P10DC_RANKFORMULA_DIRECTGATHER_SPARSE64
+#include "../gridfp/ramstream32_bucket_low_rankformula_directgather_sparse64.cuh"
+#endif
+#else
+#include "../gridfp/ramstream32_bucket_low_rankformula_nometa_directmap_depthmajor.cuh"
+#endif
+
+#define BSN_REVERSE_FUSED_TABLES_TYPE ReverseBucketZeroTables
+#define build_reverse_bucket_atomic build_reverse_bucket_atomic_release_inputs
+#if P10DC_RANKFORMULA_DIRECTGATHER64
+#if P10DC_RANKFORMULA_DIRECTGATHER_SPARSE64
+using P10DCFlatOrbitSelectedFusedDeviceTables = BucketFusedDirectHighRowsRankFormulaNometa4DirectGatherSparse64Tables;
+#else
+using P10DCFlatOrbitSelectedFusedDeviceTables = BucketFusedDirectHighRowsRankFormulaNometa4DirectGather64Tables;
+#endif
+#else
+using P10DCFlatOrbitSelectedFusedDeviceTables = BucketFusedDirectHighRowsRankFormulaNometa4DirectMapDepthMajorTables;
+#endif
+
+#if P10DC_RANKFORMULA_PRECTX_FORWARD || P10DC_RANKFORMULA_PRECTX_REVERSE
+#if P10DC_RANKFORMULA_PRECTX_COMPACT
+#define BucketFusedDeviceTables BucketFusedCompactPrecomputedHighCtxTables<P10DCFlatOrbitSelectedFusedDeviceTables>
+#define P10DC_FLAT_ORBIT_PRECTX_BYTES sizeof(P10DCHighClosureCompactPreCtx)
+#else
+#define BucketFusedDeviceTables BucketFusedPrecomputedHighCtxTables<P10DCFlatOrbitSelectedFusedDeviceTables>
+#define P10DC_FLAT_ORBIT_PRECTX_BYTES sizeof(P10DCHighClosurePreCtx)
+#endif
+#define BSN_GRAPH_BATCH_EXTRA_METADATA_BYTES(borbit,reverse) \
+    (P10DC_FLAT_ORBIT_PRECTX_BYTES * \
+     (size_t(P10DC_RANKFORMULA_PRECTX_FORWARD) * ((borbit).high_nn.size() + (borbit).high_nrnl.size()) + \
+      size_t(P10DC_RANKFORMULA_PRECTX_REVERSE) * \
+          (rattach.split.high.nn.size() + rattach.split.high.nr.size() + rattach.split.high.nl.size())))
+#else
+#define BucketFusedDeviceTables P10DCFlatOrbitSelectedFusedDeviceTables
+#endif
+
+#define BucketForwardOrbitClosureAttachHost BucketForwardPattern10DepthCodeHost
+#define BucketReverseOrbitClosureAttachHost BucketReversePattern10DepthCodeHost
+#define BucketForwardOrbitClosureAttachDeviceTables BucketForwardPattern10DepthCodeDeviceTables
+#define BucketReverseOrbitClosureAttachDeviceTables BucketReversePattern10DepthCodeDeviceTables
+#define build_bucket_forward_orbit_closure_attach build_bucket_forward_pattern10_depthcode_placeholder
+#define build_bucket_reverse_orbit_closure_attach_checked build_bucket_reverse_pattern10_depthcode_zero_checked
+#define BucketOnePassGraphs BucketPattern10DepthCodeFlatOrbitCtaDirectAffineRankFormulaNometa4AbstractGraphs
+#define bucket_onepass_graph_sync_devices bucket_pattern10_depthcode_flat_orbitcta_rankformula_nometa4_abstract_graph_sync_devices
+
+#include "oneesan_cuda_gridfp_b300_bucket_snake_onepass_graph_batch.cu"
