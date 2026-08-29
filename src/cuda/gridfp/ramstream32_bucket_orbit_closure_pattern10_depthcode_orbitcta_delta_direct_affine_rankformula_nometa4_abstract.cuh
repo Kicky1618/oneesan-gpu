5a77aabfc915d8b0c78a6ada8551a897d3863bbc
Kicky1618 <69379510+Kicky1618@users.noreply.github.com>
2026-08-29 14:57:07 +0900

    perf(gridfp): connect pair MLP to orbit-CTA ILP

--- src/cuda/gridfp/ramstream32_bucket_orbit_closure_pattern10_depthcode_orbitcta_delta_direct_affine_rankformula_nometa4_abstract.cuh
+++ src/cuda/gridfp/ramstream32_bucket_orbit_closure_pattern10_depthcode_orbitcta_delta_direct_affine_rankformula_nometa4_abstract.cuh
@@ -13,7 +13,17 @@
     p10dc_prepare_reverse_high_delta_direct_affine((c),(payload),(loc),(plan_db),(p),(edge),(ss),(js),(ds),(sr),(jr),(dr))
 #define P10DC_ORBITCTA_PLAN_SUM(c,db,lr) \
     p10dc_direct_resolved_high_plan_sum_cross5_rankformula_nometa4_abstract((c),(db),(lr))
+#if P10DC_RANKFORMULA_PAIR_MLP
+#define P10DC_ORBITCTA_PLAN_SUM_PAIR(c,db,lr0,lr1,out0,out1) \
+    p10dc_direct_resolved_high_plan_sum_pair_cross5_rankformula_nometa4_abstract( \
+        (c),(db),(lr0),(lr1),(out0),(out1))
+#define P10DC_ORBITCTA_PLAN_SUM_PAIR_LOCAL 1
+#endif
 #include "ramstream32_bucket_orbit_closure_pattern10_depthcode_orbitcta.cuh"
+#ifdef P10DC_ORBITCTA_PLAN_SUM_PAIR_LOCAL
+#undef P10DC_ORBITCTA_PLAN_SUM_PAIR_LOCAL
+#undef P10DC_ORBITCTA_PLAN_SUM_PAIR
+#endif
 #undef P10DC_ORBITCTA_PLAN_SUM
 #undef P10DC_ORBITCTA_PREPARE_REVERSE
 #undef P10DC_ORBITCTA_PREPARE_FORWARD
